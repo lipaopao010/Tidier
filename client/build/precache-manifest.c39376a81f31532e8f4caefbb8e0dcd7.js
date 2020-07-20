e9d2b0fe23ea110570c3fac6b64c639f6e76e7c3
shuo <shuo.li88@hotmail.com>
2020-07-20 12:04:44 +0800

    intial commit of basic template

---
+++ client/build/precache-manifest.c39376a81f31532e8f4caefbb8e0dcd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4be09a76dc4f3a744ad4",
+    "url": "/static/css/main.3d69b205.chunk.css"
+  },
+  {
+    "revision": "4be09a76dc4f3a744ad4",
+    "url": "/static/js/main.1a8452d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "c6e9d03574a1769445e06a222ecb588a",
+    "url": "/index.html"
+  }
+];
